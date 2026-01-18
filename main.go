9a9735f1e06e205ffa16070b1ced896a2b3d8656
boburmirzo <bobur.kozimov@gmail.com>
2026-01-18 22:19:10 +0500

    bufio read

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
-	file, err := os.ReadFile("./words.txt")
+	file, err := os.Open("./words.txt")
 	if err != nil {
-		os.Exit(1)
+		log.Fatal("failed to read file:", err)
 	}
-
-	counter := CountWords(file)
-	fmt.Println(counter)
+	defer file.Close()
+	wordsCount := CountWordsInFile(file)
+	fmt.Println(wordsCount)
 }
 
 func CountWords(data []byte) int {
 	return len(bytes.Fields(data))
+}
+
+func CountWordsInFile(file *os.File) int {
+	wordsCount := 0
+	isInsideWord := false
+	reader := bufio.NewReader(file)
+	for {
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+		if !unicode.IsSpace(r) && !isInsideWord {
+			wordsCount++
+		}
+
+		isInsideWord = !unicode.IsSpace(r)
+	}
+
+	return wordsCount
 }
