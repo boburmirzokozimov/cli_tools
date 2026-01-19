1bccd81159e3d7e89c61ef8b378cd63a4228f33c
boburmirzo <bobur.kozimov@gmail.com>
2026-01-19 11:41:25 +0500

    erro

--- main.go
+++ main.go
@@ -9,15 +9,40 @@ import (
 )
 
 func main() {
-	file, err := os.Open("./words.txt")
-	if err != nil {
-		log.Fatal("failed to read file:", err)
+
+	if len(os.Args) < 2 {
+		log.Fatalln("Should provide at least one filename")
+	}
+	total := 0
+	fileNames := os.Args[1:]
+	didErr := false
+	for _, fileName := range fileNames {
+		wordsCount, err := CountWordsInFile(fileName)
+		if err != nil {
+			fmt.Println(os.Stderr, "counter: ", err)
+			didErr = true
+			continue
+		}
+
+		fmt.Println(wordsCount, fileName)
+		total += wordsCount
+	}
+	if len(fileNames) > 1 {
+		fmt.Println(total, "total")
+	}
+	if didErr {
+		os.Exit(1)
 	}
 
-	defer file.Close()
-	wordsCount := CountWords(file)
+}
 
-	fmt.Println(wordsCount)
+func CountWordsInFile(fileName string) (int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return 0, fmt.Errorf("open file %w", err)
+	}
+	defer file.Close()
+	return CountWords(file), nil
 }
 
 func CountWords(file io.Reader) int {
